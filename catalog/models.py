1827f4bed59ed90e742aee26f0853e6f43da950b
EddieStn <eduard.eduard25@yahoo.com>
2023-03-12 09:14:49 +0000

    Add product and category model to catalog app

--- catalog/models.py
+++ catalog/models.py
@@ -1,3 +1,30 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name_plural = 'Categories'
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    image = models.ImageField(upload_to='product_images/')
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    scent = models.CharField(max_length=50)
+    size = models.CharField(max_length=20)
+    availability = models.BooleanField(default=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    rating = models.DecimalField(max_digits=3, decimal_places=2, default=0)
+    num_reviews = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.name
