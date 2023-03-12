cb27b663e9c158bcc7dbd740c083b8452618d491
EddieStn <eduard.eduard25@yahoo.com>
2023-03-12 09:24:09 +0000

    Add review model for products

--- catalog/models.py
+++ catalog/models.py
@@ -28,3 +28,12 @@ class Product(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class Review(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    content = models.TextField()
+    rating = models.IntegerField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
