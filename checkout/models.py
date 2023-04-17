51bbe67e57b37a59a954db64f0bac3d70a300211
EddieStn <eduard.eduard25@yahoo.com>
2023-04-17 19:13:04 +0000

    Build checkout app and models

---
+++ checkout/models.py
@@ -0,0 +1,46 @@
+from django.db import models
+from django.contrib.auth.models import User
+from catalog.models import Product
+
+
+class Order(models.Model):
+    STATUS_CHOICES = (
+        ('P', 'Pending'),
+        ('C', 'Confirmed'),
+        ('S', 'Shipped'),
+        ('D', 'Delivered'),
+    )
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    email = models.EmailField()
+    phone_number = models.CharField(max_length=15)
+    address_line_1 = models.CharField(max_length=255)
+    address_line_2 = models.CharField(max_length=255, blank=True)
+    city = models.CharField(max_length=50)
+    postcode = models.CharField(max_length=10)
+    country = models.CharField(max_length=50)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    status = models.CharField(max_length=1, choices=STATUS_CHOICES,
+                              default='P')
+
+    def __str__(self):
+        return f'Order #{self.id}'
+
+    def get_total_cost(self):
+        return sum(item.get_cost() for item in self.items.all())
+
+
+class OrderItem(models.Model):
+    order = models.ForeignKey(Order, on_delete=models.CASCADE,
+                              related_name='items')
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    quantity = models.PositiveIntegerField(default=1)
+
+    def __str__(self):
+        return str(self.id)
+
+    def get_cost(self):
+        return self.price * self.quantity
