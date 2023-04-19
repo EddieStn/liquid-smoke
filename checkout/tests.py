e2d80933b75e260460e66142f020328b010bc3ed
EddieStn <eduard.eduard25@yahoo.com>
2023-04-19 10:19:54 +0000

    Create TDD for the checkout view

--- checkout/tests.py
+++ checkout/tests.py
@@ -1,5 +1,9 @@
 from django.test import TestCase
+from django.urls import reverse
+from django.contrib.auth.models import User
 from checkout.forms import OrderForm
+from catalog.models import Product
+from .models import Order
 
 
 class TestOrderForm(TestCase):
@@ -50,3 +54,61 @@ class TestOrderForm(TestCase):
     def test_form_first_name_autofocus(self):
         form = OrderForm()
         self.assertTrue(form.fields['first_name'].widget.attrs['autofocus'])
+
+
+class TestCheckoutView(TestCase):
+    def setUp(self):
+        self.user = User.objects.create_user(username='testuser',
+                                             password='testpass')
+        self.product = Product.objects.create(name='Test Product', price=10)
+        self.order = Order.objects.create(
+            user=self.user, first_name='John', last_name='Doe',
+            email='johndoe@example.com', phone_number='1234567890',
+            address_line_1='123 Main St', city='New York',
+            postcode='10001', country='US')
+
+    def test_checkout_view_get(self):
+        self.client.login(username='testuser', password='testpass')
+        response = self.client.get(reverse('checkout'))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'checkout/checkout.html')
+        self.assertContains(response, 'Test Product')
+        self.assertContains(response, 'Total: $10.00')
+
+    def test_checkout_view_post(self):
+        self.client.login(username='testuser', password='testpass')
+        data = {
+            'first_name': 'John',
+            'last_name': 'Doe',
+            'email': 'johndoe@example.com',
+            'phone_number': '1234567890',
+            'address_line_1': '123 Main St',
+            'city': 'New York',
+            'postcode': '10001',
+            'country': 'US',
+            'payment_method': 'stripe',
+        }
+        response = self.client.post(reverse('checkout'), data=data)
+        self.assertRedirects(response, reverse('order_confirmation',
+                             kwargs={'order_id': self.order.id}))
+        self.assertEqual(Order.objects.count(), 2)
+
+        new_order = Order.objects.last()
+
+        self.assertEqual(new_order.user, self.user)
+        self.assertEqual(new_order.first_name, 'John')
+        self.assertEqual(new_order.last_name, 'Doe')
+        self.assertEqual(new_order.email, 'johndoe@example.com')
+        self.assertEqual(new_order.phone_number, '1234567890')
+        self.assertEqual(new_order.address_line_1, '123 Main St')
+        self.assertEqual(new_order.city, 'New York')
+        self.assertEqual(new_order.postcode, '10001')
+        self.assertEqual(new_order.country, 'US')
+        self.assertEqual(new_order.payment_method, 'stripe')
+        self.assertEqual(new_order.items.count(), 1)
+
+        order_item = new_order.items.first()
+
+        self.assertEqual(order_item.product, self.product)
+        self.assertEqual(order_item.price, self.product.price)
+        self.assertEqual(order_item.quantity, 1)
